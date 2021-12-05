ccb51ed2e19102cf6bd769f3478786b4761d33e9
CHOSION <chosion0107@gmail.com>
2021-12-05 20:30:28 +0900

    Update Ticket.py

--- no3_Ticket/Ticket.py
+++ no3_Ticket/Ticket.py
@@ -13,15 +13,15 @@ from PIL import Image, ImageDraw, ImageFont
 
 count = 142
 
-#권한 인증 및 토큰 확인
+# 권한 인증 및 토큰 확인
 SCOPES = ['https://www.googleapis.com/auth/drive']
 creds = None
 
-#토큰이 있으면
+# 토큰이 있으면
 if os.path.exists('token.json'):
     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
-#토큰이 없거나 기한이 만료되면
+# 토큰이 없거나 기한이 만료되면
 if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
         creds.refresh(Request())
@@ -33,13 +33,13 @@ if not creds or not creds.valid:
     with open('token.json', 'w') as token:
         token.write(creds.to_json())
 
-#파이프를 통해 연결. 연결 인스턴스 생성
+# 파이프를 통해 연결. 연결 인스턴스 생성
 service = build('drive', 'v3', credentials=creds)
 
 while True:
     s = input("쓰세요 : ")
     if s == 'a':
-        #음악 업로드 실행
+        # 음성 업로드 실행
         print("=====  {}번째 음성이 업로드됩니다  =====".format(count))
         file_metadata = {'name': '2021-05-23_' + str(count) + '_voice.wav', # 매일매일 이름의 날짜와 폴더ID 바꿔야합니다
                         'parents': ['10-yvjsEQ8G28jfvs_6xqjJvZSZj30xwX']}  # 5월 21일 폴더ID : 1zvjIvmhnMri7KstpOEAiuBeZBS1o8WBK
@@ -49,9 +49,9 @@ while True:
         media = MediaFileUpload('voice/' + str(count) + '_voice.wav', resumable=True)  # 파일 위치
         file = service.files().create(body=file_metadata,
                                           media_body=media,
-                                          fields='id').execute()    #파일올리는 거예요
+                                          fields='id').execute()    # 파일올리기
 
-        #QR코드 만들기
+        # QR코드 만들기
         print("=====  {}번째 QR코드 생성중  =====".format(count))
         qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L,
                                box_size=20, border=0)
@@ -60,7 +60,7 @@ while True:
         img.save("QRCode/" + str(count) + "_qrcode.png")
 
 
-        #배경화면에 옷 이미지 넣기
+        # 배경화면에 옷 이미지 넣기
         background = Image.open('0523_background.png')
         blur = Image.open('blur/' + str(count) + '_blur.png')
         blur = blur.resize((610, 528))
@@ -71,23 +71,23 @@ while True:
         ticket.paste(background, (0, 0), background)
 
 
-        #몇 번째 손님인지
+        # 몇 번째 손님인지
         draw = ImageDraw.Draw(ticket)
         font = ImageFont.truetype("../font/조선일보명조.ttf", 30)
         org = (300, 167)  # 글씨의 위치
         draw.text(org, "#%03d" % count, font=font, fill=(100, 100, 100))
 
-        #음파 넣기
+        # 음파 넣기
         wave = Image.open('waveform/' + str(count) + '_waveform.png', 'r')
         wave = wave.resize((510, 150))
         ticket.paste(wave, (65, 310), wave)
 
-        #QR코드 넣기
+        # QR코드 넣기
         QR = Image.open('QRCode/' + str(count) + '_qrcode.png')
         QR = QR.resize((120, 120))
         ticket.paste(QR, (455, 75))
 
-        #티켓 저장
+        # 티켓 저장
         print("=====  {}번째 티켓이 저장되고 있습니다  =====".format(count))
         ticket.save('Ticket/' + str(count) + "_ticket.png")
         ticket.show()
