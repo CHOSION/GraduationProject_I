f0588d47e3932579b88b19c60393589b3f5b96e4
CHOSION <chosion0107@gmail.com>
2021-12-14 00:57:24 +0900

    211214

--- no1_Camera/Camera.py
+++ no1_Camera/Camera.py
@@ -3,7 +3,11 @@ import cv2
 from sklearn.cluster import KMeans
 from PIL import ImageFont, ImageDraw, Image, ImageFilter
 
-# CAMERA SETTING
+# 카메라 세팅
 capture = cv2.VideoCapture(2)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 2160)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 4096)
+
+# 이 부분을 주석처리하면 윈도우처럼 뜬다.
+cv2.namedWindow('WINDOW_NAME', cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty('WINDOW_NAME', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
