fd86e697b91ad2580c0dd74501acd270e791f257
CHOSION <chosion0107@gmail.com>
2021-12-16 21:35:11 +0900

    Update Ticket.py

--- no3_Ticket/Ticket.py
+++ no3_Ticket/Ticket.py
@@ -9,3 +9,25 @@ from googleapiclient.http import MediaFileUpload
 import qrcode
 import keyboard
 from PIL import Image, ImageDraw, ImageFont
+
+count = 142
+
+#권한 인증 및 토큰 확인
+SCOPES = ['https://www.googleapis.com/auth/drive']
+creds = None
+
+#토큰이 있으면
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+#토큰이 없거나 기한이 만료되면
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'Credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
